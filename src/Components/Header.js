2c3de988f7d649777f1c4482238c4c5889b4982c
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-06 13:39:34 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
 
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar navbar-light" style={{backgroundColor: '#f062b2'}}>
                 <div class="container-fluid">
 
                     <a class="navbar-brand" href="#">
@@ -15,18 +15,20 @@ function Header() {
                     {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button> */}
-                    
+
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
+
+                            {/* <li class="nav-item">
                                 <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
+                            </li> */}
+
+                            {/* <li class="nav-item">
                                 <a class="nav-link" href="#">Pricing</a>
-                            </li>
+                            </li> */}
 
 
                             <li class="nav-item dropdown">
