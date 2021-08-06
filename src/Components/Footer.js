3e708b498c31033bc60edc7fb3adbdf98dfb1fab
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-06 14:03:00 +0300

    Update index.js, Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,78 @@
 function Footer() {
 
+    let date = new Date();
+
+
     return (
         <div>
+            <div className="b-example-divider"></div>
+
+
+<div className="container">
+  <footer className="py-5">
+    <div className="row">
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-4 offset-1">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of whats new and exciting from us.</p>
+          <div className="d-flex w-100 gap-2">
+            <label for="newsletter1" className="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+            <button className="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div className="d-flex justify-content-between py-4 my-4 border-top">
+
+    <div className="col-12 col-md">
+        <small className="d-block mb-3 text-muted text-center"> {date.getFullYear()} © All rights reserved </small>
+        </div>            
+   
+
+      
+      <ul className="list-unstyled d-flex">
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use href="#twitter"/></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use href="#instagram"/></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use href="#facebook"/></svg></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
 
         </div>
     )
