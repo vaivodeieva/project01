91c79d8ae2b8ff77f114ede562c8d8dacf9ece6b
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-10 14:13:31 +0300

    Update About.js

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,8 +1,12 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import {tabs, tabs2, tabs3} from '../Data/Tabs.js';
 
-export default () => (
-  <Tabs>
+function About() {
+
+    return (
+        <div>
+            <Tabs>
     <TabList>
       <Tab>1</Tab>
       <Tab>2</Tab>
@@ -20,4 +24,35 @@ export default () => (
       <h2>Any content 3</h2>
     </TabPanel>
   </Tabs>
-);
+
+        </div>
+    )
+}
+
+export default About;
+
+
+
+
+
+
+// export default () => (
+//   <Tabs>
+//     <TabList>
+//       <Tab>1</Tab>
+//       <Tab>2</Tab>
+//       <Tab>3</Tab>
+
+//     </TabList>
+
+//     <TabPanel>
+//       <h2>Any content 1</h2>
+//     </TabPanel>
+//     <TabPanel>
+//       <h2>Any content 2</h2>
+//     </TabPanel>
+//     <TabPanel>
+//       <h2>Any content 3</h2>
+//     </TabPanel>
+//   </Tabs>
+// );
