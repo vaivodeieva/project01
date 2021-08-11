f623dc10b236d53e69c119c501617152acc7ce78
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-11 21:08:04 +0300

    Update About.js

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,8 +1,73 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { tabs, tabs2, tabs3 } from '../Data/Tabs.js';
+import { NavLink } from 'react-router-dom';
 
 function About() {
+
+    const tabsElements = tabs.map((tab, index) => {
+        return (
+            <div className="row mb-5 pb-3 border-bottom " key={index}>
+                <div className="col-12 col-md-3" >
+                    <NavLink to={'/data/tabs/' + tab.id} >
+                        <img className="img-fluid" src={tab.image}  />
+                    </NavLink>
+
+                </div>
+                <div className="col-12 col-md-9">
+                    <h3>
+                        <NavLink to={'/tab/' + tab.id} className="articles-title" >{tab.title}</NavLink>
+                    </h3>
+                    <p className="text-muted">
+                        {tab.text}
+                    </p>
+                </div>
+            </div>
+        )
+    })
+
+    const tabsElements2 = tabs2.map((tab, index) => {
+        return (
+            <div className="row mb-5 pb-3 border-bottom " key={index}>
+                <div className="col-12 col-md-3" >
+                    <NavLink to={'/data/tabs/' + tab.id} >
+                        <img className="img-fluid" src={tab.image}  />
+                    </NavLink>
+
+                </div>
+                <div className="col-12 col-md-9">
+                    <h3>
+                        <NavLink to={'/tab/' + tab.id} className="articles-title" >{tab.title}</NavLink>
+                    </h3>
+                    <p className="text-muted">
+                        {tab.text}
+                    </p>
+                </div>
+            </div>
+        )
+    })
+
+    const tabsElements3 = tabs3.map((tab, index) => {
+        return (
+            <div className="row mb-5 pb-3 border-bottom " key={index}>
+                <div className="col-12 col-md-3" >
+                    <NavLink to={'/data/tabs/' + tab.id} >
+                        <img className="img-fluid" src={tab.image}  />
+                    </NavLink>
+
+                </div>
+                <div className="col-12 col-md-9">
+                    <h3>
+                        <NavLink to={'/tab/' + tab.id} className="articles-title" >{tab.title}</NavLink>
+                    </h3>
+                    <p className="text-muted">
+                        {tab.text}
+                    </p>
+                </div>
+            </div>
+        )
+    })
+
 
 
 
@@ -17,13 +82,13 @@ function About() {
                 </TabList>
 
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <h2>{tabsElements}</h2>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <h2>{tabsElements2}</h2>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                    <h2>{tabsElements3}</h2>
                 </TabPanel>
             </Tabs>
 
